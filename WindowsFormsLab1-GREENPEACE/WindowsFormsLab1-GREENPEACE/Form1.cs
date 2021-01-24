bfac26c5791a5e5fea756564668e607a530e0582
denis0872 <denispetergof@gmail.com>
2021-01-24 21:38:33 +0300

    greenpeace

---
+++ WindowsFormsLab1-GREENPEACE/WindowsFormsLab1-GREENPEACE/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab1_GREENPEACE
+{
+    public partial class Form1 : WindowsFormsLab1_GREENPEACE.Form1
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsLab1-GREENPEACE/WindowsFormsLab1-GREENPEACE/GreenPeace.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab1_GREENPEACE
+{
+    public partial class GreenPeace : Form
+    {
+        public GreenPeace()
+        {
+            InitializeComponent();
+        }
+
+        private void GreenPeace_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(105, 245),
+new Point(235, 105),
+new Point(365, 245), new Point(235, 385) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
