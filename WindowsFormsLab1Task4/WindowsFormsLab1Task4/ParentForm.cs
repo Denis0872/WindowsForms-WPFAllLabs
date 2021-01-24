e77b8b99bbe86b9cc3509827c12c734a24ed66eb
denis0872 <denispetergof@gmail.com>
2021-01-24 18:45:11 +0300

    task4-finally

---
+++ WindowsFormsLab1Task4/WindowsFormsLab1Task4/ParentForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab1Task4
+{
+    public partial class ParentForm : Form
+    { 
+        private int openDocuments = 0;
+        public ParentForm()
+        {    
+        InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+    }
+}
