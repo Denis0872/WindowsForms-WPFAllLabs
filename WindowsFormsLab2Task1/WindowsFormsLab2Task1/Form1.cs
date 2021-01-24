b5d292f1b8cfee880f7b7b136bd1030b77279c31
denis0872 <denispetergof@gmail.com>
2021-01-24 22:59:45 +0300

    Lab2task2

--- WindowsFormsLab2Task1/WindowsFormsLab2Task1/Form1.cs
+++ WindowsFormsLab2Task1/WindowsFormsLab2Task1/Form1.cs
@@ -29,11 +29,11 @@ namespace WindowsFormsLab2Task1
 
         private void btnno_MouseMove(object sender, MouseEventArgs e)
         {
-            btnno.Top -= e.Y;
-            btnno.Left += e.X;
-            if (btnno.Top < -10 || btnno.Top > 100)
+            btnno.Top += e.Y;
+            btnno.Left -= e.X;
+           if (btnno.Top < 0 || btnno.Top > 100)
                 btnno.Top = 60;
-            if (btnno.Left < -80 || btnno.Left > 250)
+            if (btnno.Left < 0 || btnno.Left > 250)
                 btnno.Left = 120;
         }
     }
