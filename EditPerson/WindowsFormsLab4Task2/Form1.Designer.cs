507d628df1fab8c1fb45cb3178fc7c21a2d76929
denis0872 <denispetergof@gmail.com>
2021-01-25 19:06:51 +0300

    Lab4Task2-finally

--- EditPerson/WindowsFormsLab4Task2/Form1.Designer.cs
+++ EditPerson/WindowsFormsLab4Task2/Form1.Designer.cs
@@ -29,28 +29,40 @@ namespace WindowsFormsLab4Task2
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.personsListView = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listView1
+            // personsListView
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.personsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(54, 21);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(271, 114);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.personsListView.HideSelection = false;
+            this.personsListView.Location = new System.Drawing.Point(54, 21);
+            this.personsListView.Name = "personsListView";
+            this.personsListView.Size = new System.Drawing.Size(271, 114);
+            this.personsListView.TabIndex = 0;
+            this.personsListView.UseCompatibleStateImageBehavior = false;
+            this.personsListView.View = System.Windows.Forms.View.Details;
+            this.personsListView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Фамилия";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Возраст";
             // 
             // button1
             // 
@@ -70,18 +82,7 @@ namespace WindowsFormsLab4Task2
             this.button2.TabIndex = 2;
             this.button2.Text = "Редактировать";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Имя";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Фамилия";
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Возраст";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -90,7 +91,7 @@ namespace WindowsFormsLab4Task2
             this.ClientSize = new System.Drawing.Size(400, 258);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.personsListView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Список сотрудников";
@@ -100,7 +101,7 @@ namespace WindowsFormsLab4Task2
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView personsListView;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ColumnHeader columnHeader1;
