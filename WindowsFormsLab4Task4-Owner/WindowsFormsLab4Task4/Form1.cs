493d0a7dd525285e3f2dad384c82c01d7642f518
denis0872 <denispetergof@gmail.com>
2021-01-27 18:09:32 +0300

    Lab4task4

---
+++ WindowsFormsLab4Task4-Owner/WindowsFormsLab4Task4/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab4Task4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string SolData
+        {
+            get
+            { return textBoxF2.Text; }
+            set
+            { textBoxF2.Text = value; }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show(this);
+        }
+    }
+}
