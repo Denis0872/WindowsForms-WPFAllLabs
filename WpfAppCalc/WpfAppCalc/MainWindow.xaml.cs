95c05c5e3a1a87db36e866bbd8754b85d816ba48
denis0872 <denispetergof@gmail.com>
2021-02-09 01:36:04 +0300

    Panel  Check-Box- connection-button

--- WpfAppCalc/WpfAppCalc/MainWindow.xaml.cs
+++ WpfAppCalc/WpfAppCalc/MainWindow.xaml.cs
@@ -23,17 +23,17 @@ namespace WpfAppCalc
     public partial class MainWindow : Window
     {
 
-      
+
         public MainWindow()
         {
             InitializeComponent();
-            AddHandler(CheckBox.CheckedEvent, new RoutedEventHandler(chk_Checked));
-            AddHandler(CheckBox.UncheckedEvent, new RoutedEventHandler(chk_Unchecked));
+            AddHandler(System.Windows.Controls.Primitives.ToggleButton.CheckedEvent, new RoutedEventHandler(chk_Checked));
+            AddHandler(System.Windows.Controls.Primitives.ToggleButton.UncheckedEvent, new RoutedEventHandler(chk_Unchecked));
             foreach (UIElement el in MainContainer.Children)
             {
-                if(el is Button)
+                if (el is Button)
                 {
-                   ((Button) el).Click += Button_Click;
+                    ((Button)el).Click += Button_Click;
                 }
             }
         }
@@ -47,7 +47,7 @@ namespace WpfAppCalc
             {
                 dynamic value = (dynamic)new DataTable().Compute(Enter.Text, null);
                 Enter.Text = value.ToString();
-                
+
             }
             else if (str == "√")
             {
@@ -57,11 +57,11 @@ namespace WpfAppCalc
             }
             else if (str == "Nⁿ")
             {
-               
+
                 double x = Convert.ToDouble(Enter.Text);
                 double y = Convert.ToDouble(Enter.Text);
 
-                double value = Math.Pow(x,y);
+                double value = Math.Pow(x, y);
                 Enter.Text = value.ToString();
             }
             else if (str == "!")
@@ -69,74 +69,74 @@ namespace WpfAppCalc
                 int x = int.Parse(Enter.Text);
                 if (x > 0)
                 {
-                    int p=1;
-                    for (int i=1; i<=x; i++)
+                    int p = 1;
+                    for (int i = 1; i <= x; i++)
                     {
-                        p*=i;
+                        p *= i;
                     }
-                int value = p;
-                Enter.Text = value.ToString();
+                    int value = p;
+                    Enter.Text = value.ToString();
                 }
                 else
                 {
                     Enter.Text = "ошибка";
                 }
-  
+
             }
             else if (str == "1/x")
             {
                 double x = Convert.ToDouble(Enter.Text);
 
-                double value = 1/x;
-                Enter.Text = value.ToString()+"   "+ DateTime.Now.ToString();
-              
+                double value = 1 / x;
+                Enter.Text = value.ToString() + "   " + DateTime.Now.ToString();
+
             }
             else if (str == "←")
             {
-                
+
                 int lenght = Enter.Text.Length - 1;
                 dynamic text1 = Enter.Text;
-                Enter.Text= "";
+                Enter.Text = "";
                 for (int i = 0; i < lenght; i++)
                 {
-                    Enter.Text +=  text1[i];
+                    Enter.Text += text1[i];
                 }
             }
             else if (str == "ⁿ²")
             {
                 dynamic x = Convert.ToDouble(Enter.Text);
-                double value =  x*x;
+                double value = x * x;
                 Enter.Text = value.ToString();
             }
-          /*  else if (str == "ⁿ")
-            {
-                 string p= Enter.Text;
-                int k = Convert.ToInt32(p);
-                //Thread.Sleep(500);
-                
-                string str2 = "5";
-                Enter.Text = str2;
-                string s = Enter.Text;
-                int n = Convert.ToInt32(s);
-                int j=0;
-                int b = 1;
-                while (j <= n)
-                { j++;b *= k; }
-                int value = b;
-                Enter.Text = value.ToString();
-            }*/
+            /*  else if (str == "ⁿ")
+              {
+                   string p= Enter.Text;
+                  int k = Convert.ToInt32(p);
+                  //Thread.Sleep(500);
+
+                  string str2 = "5";
+                  Enter.Text = str2;
+                  string s = Enter.Text;
+                  int n = Convert.ToInt32(s);
+                  int j=0;
+                  int b = 1;
+                  while (j <= n)
+                  { j++;b *= k; }
+                  int value = b;
+                  Enter.Text = value.ToString();
+              }*/
             else
                 Enter.Text += str;
         }
 
-        
+
 
         private void chk_Checked(object sender, RoutedEventArgs e)
         {
 
             CheckBox chk = e.OriginalSource as CheckBox;
-         
-            DependencyObject dpObj = LogicalTreeHelper.FindLogicalNode(this , chk.Content.ToString());
+
+            DependencyObject dpObj = LogicalTreeHelper.FindLogicalNode(this, chk.Content.ToString());
 
 
             ((FrameworkElement)dpObj).Visibility = Visibility.Visible;
@@ -153,5 +153,6 @@ namespace WpfAppCalc
         {
             Application.Current.Shutdown();
         }
+
     }
 }
